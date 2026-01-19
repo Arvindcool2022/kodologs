90797c941ecabbb809c1d17c3ff7d47766ef7edf
Arvind A <arvind.a@tolaram.com>
2026-01-19 22:35:19 +0530

    env

--- app/(main pages)/blogs/blog-grid.tsx
+++ app/(main pages)/blogs/blog-grid.tsx
@@ -1,10 +1,10 @@
-import { fetchQuery } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
+import { fetchAuthQuery } from "@/lib/auth-server";
 import BlogCard from "../../../components/blog-card";
 
 export default async function BlogsGrid() {
   await new Promise((r) => setTimeout(r, 5000)); // suspends HERE
-  const data = await fetchQuery(api.posts.getAllPosts);
+  const data = await fetchAuthQuery(api.posts.getAllPosts);
 
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
